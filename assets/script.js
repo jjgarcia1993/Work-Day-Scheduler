296accd24cd80aa6ca68b08ca90b951cb4c7b42a
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-09-03 13:45:07 -0400

    could not figure out how to correct time related color coded boxes

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,45 @@ $(document).ready(function () {
 
         localStorage.setItem(time, text);
     });
+
+    // Get item from local storage if any
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+    timeTracker();
+
+    function timeTracker() {
+        // Get the current time
+        var timeNow = moment().hour();
+
+        // Loop
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // Checking to see what colors to add
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
 });
