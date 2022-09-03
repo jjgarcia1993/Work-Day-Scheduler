c880b112b5ea32f3b7307e5c691b7268434e2793
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-09-03 13:37:12 -0400

    added time tracker

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,13 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+
+// Function for keeping track of internal time
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    });
+});
